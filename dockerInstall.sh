40ca253d6fa81d22ad59692294c9d811769c90ca
ksnithya <53986699+ksnithya@users.noreply.github.com>
2024-04-23 05:45:29 +0530

    Update dockerInstall.sh

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/bash
 yum update -y
-yum search docker
 yum install docker -y
 usermod -a -G docker ec2-user
 newgrp docker
 systemctl enable docker.service
 systemctl start docker.service
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 822626997628.dkr.ecr.ap-south-1.amazonaws.com
-docker run --name myapp -d -p 8000:5001 822626997628.dkr.ecr.ap-south-1.amazonaws.com/pipeline-demo:v1
+aws ecr get-login-password --region 822626997628.dkr.ecr.ap-south-1.amazonaws.com/pipeline-demo | docker login --username AWS --password-stdin 822626997628.dkr.ecr.ap-south-1.amazonaws.com
+declare -i x
+x=$(docker ps -a|grep -w "myapp"|wc -l)
+if [ $x -ge 1 ]
+then
+  docker stop myapp
+  docker rm myapp
+  docker run --name myapp -d -p 8000:5001 822626997628.dkr.ecr.ap-south-1.amazonaws.com/pipeline-demo:v2
+else
+  docker run --name myapp -d -p 8000:5001 822626997628.dkr.ecr.ap-south-1.amazonaws.com/pipeline-demo:v2
+fi
